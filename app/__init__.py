f90da7e31e27841fb16d95b49a3d6dc2b07f98b3
Paje <ejap99@gmail.com>
2022-11-07 02:00:45 -0600

    broken but practice w heroku

---
+++ app/__init__.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
+
+from dotenv import load_dotenv
+
+db = SQLAlchemy()
+migrate = Migrate()
+load_dotenv()
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+
+    if not test_config:
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+            "SQLALCHEMY_DATABASE_URI")
+    else:
+        app.config["TESTING"] = True
+        app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+            "SQLALCHEMY_TEST_DATABASE_URI")
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from app.models.books import Book
+    from app.models.authors import Author
+
+    from .book_routes import books_bp
+    from .author_routes import authors_bp
+
+    app.register_blueprint(books_bp)
+    app.register_blueprint(authors_bp)
+    
+    return app
+
---
+++ app/book_routes.py
@@ -0,0 +1,77 @@
+from app import db
+from app.models.books import Book
+from flask import Blueprint, jsonify, request, make_response, abort
+
+
+
+books_bp = Blueprint("books", __name__, url_prefix="/books")
+
+def validate_model(model_id):
+    try:
+        model_id = int(model_id)
+    except:
+        abort(make_response({"message":f"ID {model_id} invalid"}, 400))
+
+    model = model.query.get(model_id)
+
+    if not model:
+        abort(make_response({"message":f"ID {model_id} not found"}, 404))
+
+    return model
+
+@books_bp.route("/<book_id>", methods=["GET"])
+def handle_book(book_id):
+    book = validate_model(book_id)
+
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description
+        }
+
+@books_bp.route("/<book_id>", methods=["PUT"])
+def update_book(book_id):
+    book = validate_model(book_id)
+
+    request_body = request.get_json()
+
+    book.title = request_body["title"]
+    book.description = request_body["description"]
+
+    db.session.commit()
+
+    return make_response(f"Book #{book.id} successfully updated")
+
+@books_bp.route("/<book_id>", methods=["DELETE"])
+def delete_book(book_id):
+    book = validate_model(book_id)
+
+    db.session.delete(book)
+    db.session.commit()
+
+    return make_response(f"Book #{book.id} successfully deleted")
+
+@books_bp.route("", methods=["POST", "GET"])
+def handle_books():
+    if request.method == "GET":
+        books = Book.query.all()
+        books_response = []
+        for book in books:
+            books_response.append({
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            })
+        return jsonify(books_response)
+    elif request.method == "POST":
+        request_body = request.get_json()
+        new_book = Book(
+            title=request_body["title"],
+            description=request_body["description"]
+            )
+        db.session.add(new_book)
+        db.session.commit()
+
+    return make_response(f"Book {new_book.title} successfully created", 201)
+
+
